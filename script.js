f59875033117ba6325350661df19ccb6446a74f8
Ron Laniado <rlaniado24@gmail.com>
2017-10-08 14:23:41 -0400

    Random Article button takes you to a random wikipedia article

---
+++ script.js
@@ -0,0 +1,8 @@
+var endpoint = "https://en.wikipedia.org/w/api.php"; //Base Url for making an API request. It's essentially the homepage of the API.
+var searchUrl; //The final url before it is called in an ajax request.
+
+function searchWiki() {
+  $.ajax({
+
+  });
+}

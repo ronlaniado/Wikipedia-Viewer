b07a863cbd1248b99546d971c03b12ee35ccc8d0
Ron Laniado <rlaniado24@gmail.com>
2017-10-13 11:45:34 -0400

    Optimized code; now MUCH less repetitive

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var endpoint = "https://en.wikipedia.org/w/api.php"; //Base Url for making an API request. It's essentially the homepage of the API.
-var searchUrl = "https://en.wikipedia.org/w/api.php?action=query&prop=info&format=json&origin=*&list=search&srsearch="; //The final url before it is called in an ajax request.
+var searchUrl =
+  "https://en.wikipedia.org/w/api.php?action=query&prop=info&format=json&origin=*&list=search&srsearch="; //The final url before it is called in an ajax request.
 var articleUrl = "http://en.wikipedia.org/?curid=";
 var searchText;
 var title;
 var subtitle;
-var link;
 var title1;
 var title2;
 var title3;
@@ -29,92 +29,24 @@ var link1;
 
 function searchWiki() {
   searchText = $("#userInput").val();
-  $("#userInput").val('');
+  $("#userInput").val("");
   searchText = searchText.replace(" ", "&");
   var finalUrl = searchUrl + searchText;
-  console.log(searchText);
+  //$(".cards").empty();
   console.log(finalUrl);
   $.ajax({
     url: finalUrl,
     success: function(data) {
-      console.log("Wikipedia API ajax request was a success");
-      title1 = data.query.search[0].title;
-      subtitle1 = data.query.search[0].snippet;
-      link1 = articleUrl + data.query.search[0].pageid;
-      title2 = data.query.search[1].title;
-      subtitle2 = data.query.search[1].snippet;
-      link2 = articleUrl + data.query.search[1].pageid;
-      title3 = data.query.search[2].title;
-      subtitle3 = data.query.search[2].snippet;
-      link3 = articleUrl + data.query.search[2].pageid;
-      title4 = data.query.search[3].title;
-      subtitle4 = data.query.search[3].snippet;
-      link4 = articleUrl + data.query.search[3].pageid;
-      title5 = data.query.search[4].title;
-      subtitle5 = data.query.search[4].snippet;
-      link5 = articleUrl + data.query.search[4].pageid;
-      title6 = data.query.search[5].title;
-      subtitle6 = data.query.search[5].snippet;
-      link6 = articleUrl + data.query.search[5].pageid;
-      title7 = data.query.search[6].title;
-      subtitle7 = data.query.search[6].snippet;
-      link7 = articleUrl + data.query.search[6].pageid;
-      title8 = data.query.search[7].title;
-      subtitle8 = data.query.search[7].snippet;
-      link8 = articleUrl + data.query.search[7].pageid;
-      title9 = data.query.search[8].title;
-      subtitle9 = data.query.search[8].snippet;
-      link9 = articleUrl + data.query.search[8].pageid;
-      title10 = data.query.search[9].title;
-      subtitle10 = data.query.search[9].snippet;
-      link10 = articleUrl + data.query.search[9].pageid;
-      console.log(title1);
-      console.log(link1);
-      $(".card-title1").empty();
-      $(".card-title2").empty();
-      $(".card-subtitle1").empty();
-      $(".card-subtitle2").empty();
-      $(".card-title3").empty();
-      $(".card-subtitle3").empty();
-      $(".card-title4").empty();
-      $(".card-subtitle4").empty();
-      $(".card-title5").empty();
-      $(".card-subtitle5").empty();
-      $(".card-title6").empty();
-      $(".card-subtitle6").empty();
-      $(".card-title7").empty();
-      $(".card-subtitle7").empty();
-      $(".card-title8").empty();
-      $(".card-subtitle8").empty();
-      $(".card-title9").empty();
-      $(".card-subtitle9").empty();
-      $(".card-title10").empty();
-      $(".card-subtitle10").empty();
-      $(".card-title1").append(title1);
-      $(".card-title2").append(title2);
-      $(".card-subtitle1").append(subtitle1 + "...");
-      $(".card-subtitle2").append(subtitle2 + "...");
-      $(".card-title3").append(title3);
-      $(".card-subtitle3").append(subtitle3 + "...");
-      $(".card-title4").append(title4);
-      $(".card-subtitle4").append(subtitle4 + "...");
-      $(".card-title5").append(title5);
-      $(".card-subtitle5").append(subtitle5 + "...");
-      $(".card-title6").append(title6);
-      $(".card-subtitle6").append(subtitle6 + "...");
-      $(".card-title7").append(title7);
-      $(".card-subtitle7").append(subtitle7 + "...");
-      $(".card-title8").append(title8);
-      $(".card-subtitle8").append(subtitle8 + "...");
-      $(".card-title9").append(title9);
-      $(".card-subtitle9").append(subtitle9 + "...");
-      $(".card-title10").append(title10);
-      $(".card-subtitle10").append(subtitle10 + "...");
-      for (var i = 0; i < data.query.search[i].length; i++) {
+      console.log(data.query.search.length);
+      for (var i = 0; i < data.query.search.length; i++) {
         title = data.query.search[i].title;
         subtitle = data.query.search[i].snippet;
-        console.log(title);
-        console.log(subtitle);
+        //console.log(title);
+        //console.log(subtitle);
+        var titleGetter = document.getElementById("title" + (i + 1));
+        $(titleGetter).html(title);
+        var subtitleGetter = document.getElementById("subtitle" + (i + 1));
+        $(subtitleGetter).html(subtitle);
       }
     }
   });
@@ -125,11 +57,13 @@ document.getElementById("userInput").onkeypress = function(e) {
     e.preventDefault();
   }
 };
-$.fn.extend({ //function that makes it easy to do animations
+$.fn.extend({
+  //function that makes it easy to do animations
   animateCss: function(animationName) {
-    var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-    this.addClass('animated ' + animationName).one(animationEnd, function() {
-      $(this).removeClass('animated ' + animationName);
+    var animationEnd =
+      "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
+    this.addClass("animated " + animationName).one(animationEnd, function() {
+      $(this).removeClass("animated " + animationName);
     });
     return this;
   }
@@ -137,11 +71,14 @@ $.fn.extend({ //function that makes it easy to do animations
 document.addEventListener("keyup", function(event) {
   if (event.keyCode == 13) {
     event.preventDefault(); //Disables the form from being submitted
-    $("#userInput").animate({ //animates the search box
-      marginTop: "1.5%",
-      height: "35px",
-      fontSize: "18px",
-    }, 1000);
+    $("#userInput").animate({
+        //animates the search box
+        marginTop: "1.5%",
+        height: "35px",
+        fontSize: "18px"
+      },
+      1000
+    );
     searchWiki();
     document.querySelector(".card").style.display = "block"; //makes the card not hidden
     $(".card").animateCss("fadeInLeft"); //makes the card fade in left
@@ -186,5 +123,5 @@ document.addEventListener("keyup", function(event) {
 });
 
 function cardLink1() {
-  window.open(link1, '_blank');
+  window.open(link1, "_blank");
 }
